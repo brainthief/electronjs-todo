43831731227f3cf945ef085d9cbdf19b373ff204
brainthief <vytautas1983@gmail.com>
2020-12-27 22:51:31 +0200

    add clear todo

--- index.js
+++ index.js
@@ -46,6 +46,12 @@ const menuTemplate = [
     },
    },
    {
+    label: "Clear Todo",
+    click() {
+     mainWindow.webContents.send("todo:clear")
+    },
+   },
+   {
     label: "Quit",
     accelerator: process.platform === "darwin" ? "Command+Q" : "Ctrl+Q",
     click() {
